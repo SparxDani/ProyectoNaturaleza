ff16c5e4397a00baa42e0738ae037f2cabf8ce17
Brando123412 <130480267+Brando123412@users.noreply.github.com>
2023-11-11 17:09:21 -0500

    sdsad

--- Assets/BrandoNiels/Scripts/GeneradorBasuraController.cs
+++ Assets/BrandoNiels/Scripts/GeneradorBasuraController.cs
@@ -7,15 +7,30 @@ public class GeneradorBasuraController : MonoBehaviour
     public List<BasuraController> basurasGeneradas =new List<BasuraController>();
     [SerializeField] BasuraController[] basurasPrefabs;
     [SerializeField]int quantity;
+    [SerializeField] GameController[] spawnPositionBasura;
+    int posX,posY, randomBasura,randomPositionBasura;
+    BasuraController sabeBasura;
+    Vector2 positiotmp;
+
     void Start()
     {
         for (int i = 0; i < quantity; i++)
         {
-            
+            posX= Random.Range(-5,5);
+            posY= Random.Range(-5,5);
+            randomPositionBasura= Random.Range(0,spawnPositionBasura.Length);
+            randomBasura = Random.Range(0,basurasPrefabs.Length);
+
+            positiotmp = new Vector2(spawnPositionBasura[randomPositionBasura].transform.position.x+posX,
+            spawnPositionBasura[randomPositionBasura].transform.position.y+posY);
+
+            sabeBasura = Instantiate(basurasPrefabs[randomBasura],positiotmp,Quaternion.identity);
+            basurasGeneradas.Add(sabeBasura);
         }
     }
     void Update()
     {
         
     }
+    
 }
--- Assets/BrandoNiels/Scripts/PlayerController.cs
+++ Assets/BrandoNiels/Scripts/PlayerController.cs
@@ -7,9 +7,39 @@ public class PlayerController : MonoBehaviour
 {
     List<BasuraController> basurasRecogidas = new List<BasuraController>();
     [SerializeField] List<BasuraController> basurasgenerales = new List<BasuraController>();
+    GeneradorBasuraController generadorBasuraController;
+    [SerializeField] float speed;
+    public float horizontal, vertical;
+    bool horizontalBoolN,horizontalBoolP=false;
+    bool verticalBoolN,verticalBoolP=false;
+    Rigidbody2D rb2d;
+    
+    
+    private void Awake() {
+        rb2d=GetComponent<Rigidbody2D>();
+    }
+    void Update() {
+        InputMovement();
+        if(horizontalBoolN){
+            horizontal=-1;
+        }else if(horizontalBoolP){
+            horizontal=1;
+        }
+        if(verticalBoolN){
+            vertical=-1;
+        }else if(verticalBoolP){
+            vertical=1;
+        }
+    }
+    private void FixedUpdate() {
+        
+        Movemet();
+    }
     void OnTriggerEnter2D(Collider2D other){
         if(other.CompareTag("Basura")){
+            other.gameObject.SetActive(false);
             basurasRecogidas.Add(other.gameObject.GetComponent<BasuraController>());
+            
         }
         if(other.CompareTag("Recojedor")){
             ReciclarBasura(other.gameObject);
@@ -23,7 +53,10 @@ public class PlayerController : MonoBehaviour
             BuscarBasura("basura2",recojedor.transform);
         }else if(nameRecojedor == "Recojedor3"){
             BuscarBasura("basura3",recojedor.transform);
+        }else if(nameRecojedor == "Recojedor4"){
+            BuscarBasura("basura4",recojedor.transform);
         }
+        
     }
     void BuscarBasura(string nombreBasura,Transform positionTacho){
 
@@ -34,10 +67,45 @@ public class PlayerController : MonoBehaviour
             if (basurasRecogidas[i].nombre == nombreBasura)
             {
                 basuraEncontrada = basurasRecogidas[i];
+                basuraEncontrada.gameObject.SetActive(true);
                 basuraEncontrada.transform.position = Vector3.MoveTowards(basuraEncontrada.transform.position, positionTacho.position, 3 * Time.deltaTime);
                 basurasRecogidas.RemoveAt(i);
                 basuraEncontrada.gameObject.SetActive(false);
             }
         }
+    }
+    void InputMovement(){
+        if(horizontal==0){
+            vertical = Input.GetAxis("Vertical");
+        }
+        if(vertical==0){
+            horizontal = Input.GetAxis("Horizontal");
+        }
+    }
+    void Movemet(){
+        rb2d.velocity=new Vector2(horizontal*speed,vertical*speed);
+    }
+    public void ClickLeft(){
+        horizontalBoolN = true;
+    }
+    public void ReleaseHorizontal(){
+        horizontalBoolP = false;
+        horizontalBoolN =false;
+    }
+    public void ClickRight(){
+        horizontalBoolP = true;
+    }
+
+
+    
+    public void ClickUp(){
+        verticalBoolP = true;
+    }
+    public void ReleaseVertical(){
+        verticalBoolP = false;
+        verticalBoolN =false;
+    }
+    public void ClickDownd(){
+        verticalBoolN = true;
     }
 }
